9aaa7c5ef402dd8b328425f6d1cbb85330a9025a
liuboheng <lbh9311@sina.com>
2019-10-23 23:51:34 +0800

    update

--- P0-Multi_client_echo_server/implement/p0/server_impl.go
+++ P0-Multi_client_echo_server/implement/p0/server_impl.go
@@ -28,7 +28,7 @@ func (s *multiEchoServer) Start(port int) error {
 	if err != nil {
 		return err
 	}
-
+	defer s.Close()
 	for {
 		conn, err := ln.Accept()
 		if err!= nil {
@@ -39,6 +39,8 @@ func (s *multiEchoServer) Start(port int) error {
 }
 
 func (s *multiEchoServer) handleRequest(conn net.Conn) {
+	// TODO: go 的队列维护
+	s.addConnection(conn)
 	for {
 		buf := make([]byte,4096)
 		n, err := conn.Read(buf)
@@ -47,13 +49,16 @@ func (s *multiEchoServer) handleRequest(conn net.Conn) {
 			break
 		}
 		fmt.Println("msg length is",n)
-		fmt.Println(string(buf[0:n]))
+		fmt.Println("content is:", string(buf[0:n]))
+		conn.Write([]byte("receive"))
 	}
 	//buf.ReadString('\n')
+	defer conn.Close()
 }
 
 func (s *multiEchoServer) addConnection(conn net.Conn) {
-	s.conns = append()
+	s.conns = append(s.conns,conn)
+	s.count = s.count + 1
 }
 
 
@@ -62,5 +67,10 @@ func (s *multiEchoServer) Count() int {
 }
 
 func (s *multiEchoServer) Close() {
+	for i:=0;i<len(s.conns);i++ {
+		s.conns[i].Write([]byte("end"))
+		s.conns[i].Close()
+	}
 	return
 }
+
